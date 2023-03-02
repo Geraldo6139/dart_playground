a8c23140d32ffd0f369cbf30c49fd62ba2e5832b
Geraldo <gn6139@gmail.com>
2023-03-02 19:53:42 -0300

    Colocando enunciado

--- dart_playground/bin/calcula_imc.dart
+++ dart_playground/bin/calcula_imc.dart
@@ -1,5 +1,10 @@
 import 'dart:io';
+/*DESAFIO MASTERCLASS
 
+O (IMC) índice de massa corporal é baseado na altura e peso do individuo.
+O calculo é representado por Dividir o peso pela Altura ao quadradro²
+
+Cria uma função que realize o calculo de IMC. */
 
 void main(){
 
